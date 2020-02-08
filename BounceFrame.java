fa2c3cac87c59c4e19e98dc926c277bd3337c52f
fream_dream <nastyonanoname@gmail.com>
2020-02-08 23:00:24 +0200

    task1

--- BounceFrame.java
+++ BounceFrame.java
@@ -8,30 +8,18 @@ public class BounceFrame extends JFrame {
     private BallCanvas canvas;
     public static final int WIDTH = 450;
     public static final int HEIGHT = 350;
-Color c1 = new Color (255,255,0);
+    Color c1 = new Color (255,255,0);
     Color c2 = new Color (0,100,255);
     Color c3 = new Color (0,102,50);
     public BounceFrame() {
         this.setSize(WIDTH, HEIGHT);
         this.setTitle("Bounce programm");
-
-
-
-
-
-        this.canvas = new BallCanvas();
-
-
-
+         this.canvas = new BallCanvas();
         System.out.println("In Frame Thread name = "
                 + Thread.currentThread().getName());
         this.canvas.setBackground(c3);
         Container content = this.getContentPane();
-
         content.add(this.canvas, BorderLayout.CENTER);
-
-
-
         JPanel buttonPanel = new JPanel();
         buttonPanel.setBackground(Color.lightGray);
 
@@ -45,7 +33,6 @@ Color c1 = new Color (255,255,0);
         canvas.addC(con);
 
         buttonStart.addActionListener(new ActionListener() {
-
             @Override
             public void actionPerformed(ActionEvent e) {
 
@@ -57,9 +44,7 @@ Color c1 = new Color (255,255,0);
                 thread.setPriority(Thread.MAX_PRIORITY);
                 thread.start();
                 System.out.println("Thread name = " + thread.getName());
-
                 canvas.repaint();
-
             }
         });
 
