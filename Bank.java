f81282ad8b2cbfa01c07041215b3ac16772e200f
fream_dream <nastyonanoname@gmail.com>
2020-02-25 11:06:33 +0200

    lab2

--- Bank.java
+++ Bank.java
@@ -1,35 +1,33 @@
-class Bank {
-
+public class Bank {
     public static final int NTEST = 10000;
-    private final int[] accounts;
+    private final int[] accounts;               // масив рахунків
     private long ntransacts = 0;
 
     public Bank(int n, int initialBalance){
-        accounts = new int[n];
+        accounts = new int[n];                  // створити рахунок в банку
         int i;
-        for (i = 0; i < accounts.length; i++)
+        for (i = 0; i < accounts.length; i++)   // нарахувати на кожний рахунок 10 000
             accounts[i] = initialBalance;
         ntransacts = 0;
-    }
 
-    public void transfer(int from, int to, int amount)
-            throws InterruptedException{
+    }
+    public void transfer(int from, int to, int amount) throws InterruptedException{
         accounts[from] -= amount;
         accounts[to] += amount;
         ntransacts++;
         if (ntransacts % NTEST == 0)
             test();
+
     }
     public void test(){
         int sum = 0;
         for (int i = 0; i < accounts.length; i++)
             sum += accounts[i] ;
-        System.out.println("Transactions:" + ntransacts
+        System.out.println("Transactions: " + ntransacts
                 + " Sum: " + sum);
-    }
 
+    }
     public int size(){
         return accounts.length;
     }
 }
-
--- TransferThread.java
+++ TransferThread.java
@@ -1,8 +1,9 @@
-class TransferThread extends Thread {
+
+public class TransferThread extends Thread {
     private Bank bank;
     private int fromAccount;
     private int maxAmount;
-    private static final int REPS = 1000;
+    private static final int REPS = 1000; // кількість транзакцій
 
     public TransferThread(Bank b, int from, int max){
         bank = b;
@@ -13,9 +14,8 @@ class TransferThread extends Thread {
         try{
             while (!interrupted()){
                 for (int i = 0; i < REPS; i++){
-                    int toAccount = (int)(bank.size()*Math.random());
-                    int amount =
-                            (int)(maxAmount * Math.random()/REPS);
+                    int toAccount = (int)(bank.size() * Math.random());     // куди переводимо
+                    int amount = (int)(maxAmount * Math.random() / REPS);   // кількість грошей, яку переводимо
                     bank.transfer(fromAccount, toAccount, amount);
                     Thread.sleep(1);
                 }
